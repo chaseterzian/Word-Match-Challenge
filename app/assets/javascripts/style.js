3966cfde0778e7995890d223131068e6d1410f4d
ChaseTerzian <chaseterzian@gmail.com>
2015-09-23 23:57:35 -0600

    1 letter match test works, easily broken with anything other than 1 letter for now

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	
 	var userWordStored = [];
+	var computerGuess = "a";
+	var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 	$('#submit-button').on('click', function() {
 		var userInput = document.getElementById('user-word-input').value
@@ -8,10 +10,15 @@ $(document).ready(function() {
 
 		var userWordDisplay = document.getElementById("user-word-display");
 		userWordDisplay.innerHTML = userWordStored;
+
+		var computerGuessPrintout = document.getElementById("computer-guess");
+		var guessInfo = document.getElementById("guess-info");
+		for (var i=0; userWordStored[0] !== computerGuess; i++) {
+			computerGuess = alphabet[i];
+			computerGuessPrintout.innerHTML = computerGuess;
+			guessInfo.innerHTML = 'it took me ' + i + ' guesses';
+		}
 		
-		
-		// var computerGuess = document.getElementById("computer-guess");
-		// computerGuess.innerHTML = userWordStored;
 	});
 
 
