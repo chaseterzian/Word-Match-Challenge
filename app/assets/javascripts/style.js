4336c93701ce5eebd5b4f5c490298d1f213c9a39
ChaseTerzian <chaseterzian@gmail.com>
2015-09-24 00:41:31 -0600

    still only works with 1 letter but typing other things will not break the program anymore

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	
 	var userWordStored = [];
 	var computerGuess = "a";
-	var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 	$('#submit-button').on('click', function() {
 		var userInput = document.getElementById('user-word-input').value
@@ -13,12 +13,31 @@ $(document).ready(function() {
 
 		var computerGuessPrintout = document.getElementById("computer-guess");
 		var guessInfo = document.getElementById("guess-info");
-		for (var i=0; userWordStored[0] !== computerGuess; i++) {
+		
+		for (var i=0; i<alphabet.length; i++) {
 			computerGuess = alphabet[i];
-			computerGuessPrintout.innerHTML = computerGuess;
-			guessInfo.innerHTML = 'it took me ' + i + ' guesses';
+			guessInfo.innerHTML = 'Im thinking';
+			if (computerGuess === userWordStored[0]) {
+				guessInfo.innerHTML = 'it took me ' + (i+1) + ' guesses';
+				computerGuessPrintout.innerHTML = computerGuess;
+				return;
+			}
 		}
 		
+			// if (i >= alphabet.length) {
+				// for (var x=0; userWordStored[0] !== computerGuess; x++) {
+					// computerGuess = alphabet[i]
+					// for (var y=0; userWordStored[0] !== computerGuess; y++) {
+
+					// }
+					// computerGuessPrintout.innerHTML = computerGuess;
+					// guessInfo.innerHTML = 'it took me ' + i + ' guesses';
+				// }
+
+			
+			// }
+
+		// }
 	});
 
 
