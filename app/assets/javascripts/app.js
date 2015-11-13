f2714c7b9365e869e3a5d8024c1e33e94cae0847
ChaseTerzian <chaseterzian@gmail.com>
2015-11-12 21:20:06 -0700

    now accepts a string of 2 letters, numbers, and/or symbols, with an extra bank of misc symbols - will connect as an option later

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
 	
 	alphabet = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I', 
 	            'j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R', 
-	            's','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z'];
-	            console.log(alphabet.length);
+	            's','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z',
+	            '0','1','2','3','4','5','6','7','8','9',
+	            '!','@','#','$','%','^','&','*','(',')'];
+	extraSymbols = ['-','_','+','=','[','{',']','}','\'','|',';',':',"'",'"','<','>','?','`','~'];
+
 	var userWordStored = [];
 	var computerGuessing = "a";
 	var computerGuessPrintout = document.getElementById("computer-guess");
@@ -12,12 +15,9 @@ $(document).ready(function() {
 	$('#submit-button').on('click', function() {
 		var userInput = document.getElementById('user-word-input').value
 		userWordStored.push(userInput);//Stores user input value
-		
+
 		var userWordDisplay = document.getElementById("user-word-display");
 		userWordDisplay.innerHTML = userWordStored;//Shows users word
-
-
-
 		
 		for (var i=0; i<alphabet.length; i++) {
 			computerGuessing = alphabet[i];
@@ -27,14 +27,13 @@ $(document).ready(function() {
 				computerGuessPrintout.innerHTML = computerGuessing;
 				return;
 			}
-		}
+		}//1character
 
 
 
 		
 		for (var i=0; i<alphabet.length; i++) {
 			for (var x=0; x<alphabet.length; x++) {
-				
 				computerGuessing = alphabet[i] + alphabet[x];
 				guessInfo.innerHTML = 'This 2 letter word stumps me..';
 					
@@ -42,50 +41,17 @@ $(document).ready(function() {
 						
 						if (i===0) { 
 							guessInfo.innerHTML = 'it took me ' + ( 52+x+1 ) + ' tries'; 
-							console.log("inside i===0");
-							console.log(i);
-							console.log(x);
-							console.log("inside i===0");
-							console.log(computerGuessing);
-							console.log(userWordStored);
 							computerGuessPrintout.innerHTML = computerGuessing;
-
-
 							return;
 						}
-
-						console.log("when i isnt 0");
-						console.log(i);
-						console.log(x);
-						console.log("when i isnt 0");
-						console.log(computerGuessing);
-						console.log(userWordStored);
-
-
+						
 						guessInfo.innerHTML = 'it took me ' + ( (52*i+52) + (x+1) ) + ' tries';
-						
 						computerGuessPrintout.innerHTML = computerGuessing;
-						
 						return;
 					}
 			}
-		}
+		}//2characters
 
-		
-			// if (i >= alphabet.length) {
-				// for (var x=0; userWordStored[0] !== computerGuess; x++) {
-					// computerGuess = alphabet[i]
-					// for (var y=0; userWordStored[0] !== computerGuess; y++) {
-
-					// }
-					// computerGuessPrintout.innerHTML = computerGuess;
-					// guessInfo.innerHTML = 'it took me ' + i + ' guesses';
-				// }
-
-			
-			// }
-
-		// }
 	});
 
 
