a48a1ab5e443ce4a8f79d03ee802aab5d5b7b0bc
ChaseTerzian <chaseterzian@gmail.com>
2015-11-20 20:32:17 -0700

    improved dict speed 4x from v1

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,6 +7,9 @@ function dictionaryMode(scanMode) {
 	var userWordStored = [];
 	var computerGuessing = "a";
 	var computerGuessing2 = "b";
+	var computerGuessing3 = "c";
+	var computerGuessing4 = "d";
+	var computerGuessing5 = "e";
 	
 	var computerGuessPrintout = document.getElementById("computer-guess");
 	var guessInfo = document.getElementById("guess-info");
@@ -15,33 +18,68 @@ function dictionaryMode(scanMode) {
 	var userWordDisplay = document.getElementById("user-word-display");
 		userWordDisplay.innerHTML = userWordStored;//Shows users word
 
-		for (var i=0; i<scanMode.length; i=i+2) {
-			tryCount = tryCount+2;
+		for (var i=0; i<scanMode.length; i=i+5) {
+			tryCount = tryCount+4;//revise
 			computerGuessing = scanMode[i];
 			computerGuessing2 = scanMode[i+1];
+			computerGuessing3 = scanMode[i+2];
+			computerGuessing4 = scanMode[i+3];
+			computerGuessing5 = scanMode[i+4];
 			guessInfo.innerHTML = 'Dictionary scan could not find a positive match. Please wait while the default backup scan is running. This may take up to several minutes.';
 			
 			if (computerGuessing === userWordStored[0]) {
 				endTimeInMs = new Date().getTime();//V
 				searchDurationInMs = endTimeInMs - startTimeInMs;//V
-				guessInfo.innerHTML = 'it took me ' + tryCount + ' tries in ' + searchDurationInMs + ' ms';
+				guessInfo.innerHTML = 'it took me ' + (tryCount-4) + ' tries in ' + searchDurationInMs + ' ms';
 				computerGuessPrintout.innerHTML = computerGuessing;
 
 				setTimeout(function(){
 					location.reload();
-				}, 5000);
+				}, 8000);
 				return;
 			}
-
 			if (computerGuessing2 === userWordStored[0]) {
 				endTimeInMs = new Date().getTime();//V
 				searchDurationInMs = endTimeInMs - startTimeInMs;//V
-				guessInfo.innerHTML = 'it took me ' + tryCount + ' tries in ' + searchDurationInMs + ' ms';
-				computerGuessPrintout.innerHTML = computerGuessing;
+				guessInfo.innerHTML = 'it took me ' + (tryCount-3) + ' tries in ' + searchDurationInMs + ' ms';
+				computerGuessPrintout.innerHTML = computerGuessing2;
 
 				setTimeout(function(){
 					location.reload();
-				}, 5000);
+				}, 8000);
+				return;
+			}
+			if (computerGuessing3 === userWordStored[0]) {
+				endTimeInMs = new Date().getTime();//V
+				searchDurationInMs = endTimeInMs - startTimeInMs;//V
+				guessInfo.innerHTML = 'it took me ' + (tryCount-2) + ' tries in ' + searchDurationInMs + ' ms';
+				computerGuessPrintout.innerHTML = computerGuessing3;
+
+				setTimeout(function(){
+					location.reload();
+				}, 8000);
+				return;
+			}
+			if (computerGuessing4 === userWordStored[0]) {
+				endTimeInMs = new Date().getTime();//V
+				searchDurationInMs = endTimeInMs - startTimeInMs;//V
+				guessInfo.innerHTML = 'it took me ' + (tryCount-1) + ' tries in ' + searchDurationInMs + ' ms';
+				computerGuessPrintout.innerHTML = computerGuessing4;
+
+				setTimeout(function(){
+					location.reload();
+				}, 8000);
+				return;
+			}
+			if (computerGuessing5 === userWordStored[0]) {
+				endTimeInMs = new Date().getTime();//V
+				searchDurationInMs = endTimeInMs - startTimeInMs;//V
+				guessInfo.innerHTML = 'it took me ' + tryCount + ' tries in ' + searchDurationInMs + ' ms';
+				computerGuessPrintout.innerHTML = computerGuessing5;
+
+				setTimeout(function(){
+					location.reload();
+				}, 8000);
 				return;
 			}
 		}
@@ -59,6 +97,7 @@ function infoStream(scanMode) {
 	var tryCount = 0;
 	var userWordStored = [];
 	var computerGuessing = "a";
+	var computerGuessing2 = "b";
 	
 	var computerGuessPrintout = document.getElementById("computer-guess");
 	var guessInfo = document.getElementById("guess-info");
